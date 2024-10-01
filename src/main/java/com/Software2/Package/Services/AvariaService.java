196c741bb7e11b6f35c0694ccbdcfa6c04b8c3bf
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-10-01 15:03:39 -0500

    fixing averiaService

--- src/main/java/com/Software2/Package/Services/AvariaService.java
+++ src/main/java/com/Software2/Package/Services/AvariaService.java
@@ -12,68 +12,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AvariaService implements Averiarepository {
+public class AvariaService  {
 
     @Autowired
     private Averiarepository Averiarepository;
 
     @Transactional
-    @Override
+
     public <S extends Averia> S save(S entity) {
 
         return Averiarepository.save(entity);
     }
 
-    @Override
+
     public <S extends Averia> Iterable<S> saveAll(Iterable<S> entities) {
         return Averiarepository.saveAll(entities);
     }
 
-    @Override
+
     public Optional<Averia> findById(Long id) {
         return Averiarepository.findById(id);
     }
 
-    @Override
+
     public boolean existsById(Long id) {
         return Averiarepository.existsById(id);
     }
 
-    @Override
+
     public Iterable<Averia> findAll() {
         return Averiarepository.findAll();
     }
 
-    @Override
+
     public Iterable<Averia> findAllById(Iterable<Long> ids) {
         return Averiarepository.findAllById(ids);
     }
 
-    @Override
-    public long count() {
-        return 0;
-    }
 
     @Transactional
-    @Override
+
     public void deleteById(Long id) {
         Averiarepository.deleteById(id);
     }
 
-    @Override
-    public void delete(Averia entity) {
-    }
 
-    @Override
-    public void deleteAllById(Iterable<? extends Long> ids) {
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends Averia> entities) {
-    }
-
-    @Override
-    public void deleteAll() {
-    }
 
 }
