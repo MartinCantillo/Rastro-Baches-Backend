f58f358e6590683bf61789d84c91e913767b67c8
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-10-01 15:05:20 -0500

    fixing BacheService

--- src/main/java/com/Software2/Package/Services/BacheService.java
+++ src/main/java/com/Software2/Package/Services/BacheService.java
@@ -15,69 +15,69 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BacheService implements BacheRepository {
+public class BacheService {
 
     @Autowired
     private BacheRepository BacheRepository;
 
     @Transactional
-    @Override
+
     public <S extends Bache> S save(S entity) {
         return BacheRepository.save(entity);
     }
 
-    @Override
+
     public <S extends Bache> Iterable<S> saveAll(Iterable<S> entities) {
         return null;
     }
 
-    @Override
+
     public Optional<Bache> findById(Long id) {
         return BacheRepository.findById(id);
     }
 
-    @Override
+
     public boolean existsById(Long id) {
         return BacheRepository.existsById(id);
     }
 
-    @Override
+
     public Iterable<Bache> findAll() {
         return BacheRepository.findAll();
     }
 
-    @Override
+
     public Iterable<Bache> findAllById(Iterable<Long> ids) {
         return BacheRepository.findAllById(ids);
     }
 
-    @Override
+
     public long count() {
         return 0;
     }
 
     @Transactional
-    @Override
+
     public void deleteById(Long id) {
         BacheRepository.deleteById(id);
     }
 
-    @Override
+
     public void delete(Bache entity) {
     }
 
-    @Override
+
     public void deleteAllById(Iterable<? extends Long> ids) {
     }
-    @Override
+
     public void deleteAll(Iterable<? extends Bache> entities) {
     }
 
-    @Override
+
     public void deleteAll() {
     }
 
-    @Override
+
     public List<Bache> findByciudadano(Ciudadano ciudadano) {
         return BacheRepository.findByciudadano(ciudadano);
     }
