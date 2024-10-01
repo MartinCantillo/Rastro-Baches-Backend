fc86d6156b25deca11d396d1b4309da5ea4f5fdc
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-10-01 15:10:49 -0500

    fixing PersonalService

--- src/main/java/com/Software2/Package/Services/PersonalService.java
+++ src/main/java/com/Software2/Package/Services/PersonalService.java
@@ -14,73 +14,72 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PersonalService implements PersonalRepository {
+public class PersonalService  {
 
     @Autowired
     private PersonalRepository PersonalRepository;
 
     @Transactional
-    @Override
+
     public <S extends Personal> S save(S entity) {
         return PersonalRepository.save(entity);
     }
 
-    @Override
+
     public <S extends Personal> Iterable<S> saveAll(Iterable<S> entities) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
+
     public Optional<Personal> findById(Long id) {
         return PersonalRepository.findById(id);
     }
 
-    @Override
+
     public boolean existsById(Long id) {
         return PersonalRepository.existsById(id);
     }
 
-    @Override
+
     public Iterable<Personal> findAll() {
         return PersonalRepository.findAll();
     }
 
-    @Override
+
     public Iterable<Personal> findAllById(Iterable<Long> ids) {
         return PersonalRepository.findAllById(ids);
     }
 
-    @Override
+
     public long count() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-      @Transactional
-    @Override
+    @Transactional
     public void deleteById(Long id) {
 PersonalRepository.deleteById(id);    }
 
-    @Override
+
     public void delete(Personal entity) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
+
     public void deleteAllById(Iterable<? extends Long> ids) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
+
     public void deleteAll(Iterable<? extends Personal> entities) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
+
     public void deleteAll() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
+
     public List<Personal> findByorden(Orden orden) {
         return PersonalRepository.findByorden(orden);
     }
